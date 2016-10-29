64dade70669340fd2407a464bfbfc2bb2c8bec6a
loitp <www.muathu@gmail.com>
2016-10-29 23:41:35 +0700

    Finish game

---
+++ TicTacToe/app/src/main/java/tictactoe/ngontinhkangkang/com/tictactoe/GameActivity.java
@@ -0,0 +1,267 @@
+package tictactoe.ngontinhkangkang.com.tictactoe;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class GameActivity extends AppCompatActivity {
+    private Button[] button;
+    private int[] enableButtons;
+    private TextView tvScoreComputer, tvScoreMe;
+    private GameEngine game;
+    private final static int numberOfButton = 9;//The number of buttons on screen
+    private final static int addAPoint = 1;
+    private Handler handler;
+    private boolean checkRunnableAdded;
+    private SharedPreferences saveGameScore;
+    private static final String storeAndroidScore = "storeAndroidScore";
+    private static final String storeMeScore = "storeMeScore";
+    private static final String checkFirsTime = "checkFirstTimes";
+    private int scoreComputer, scoreMe; //Holding scores value
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        referenceToButton();
+        tvScoreComputer = (TextView) findViewById(R.id.displayScoreAndroid);
+        tvScoreMe = (TextView) findViewById(R.id.displayScoreMe);
+        game = new GameEngine(numberOfButton);
+        handler = new Handler();
+        checkRunnableAdded = false;
+        saveGameScore = getSharedPreferences(storeAndroidScore, 0);
+        scoreComputer = saveGameScore.getInt(storeAndroidScore, 0);
+        scoreMe = saveGameScore.getInt(storeMeScore, 0);
+        displayScore();
+        if (firstTimeShowInstruction()) {
+            showHowToPlay();
+        }
+        startNewGame();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    private void rateApp(String packageName) {
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + packageName)));
+        } catch (android.content.ActivityNotFoundException anfe) {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=" + packageName)));
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.rate_app:
+                rateApp(getPackageName());
+                break;
+            case R.id.more_app:
+                String nameOfDeveloper = "NgonTinh KangKang";
+                String uri = "https://play.google.com/store/apps/developer?id=" + nameOfDeveloper;
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                startActivity(i);
+                showHowToPlay();
+                break;
+            case R.id.action_info:
+                showHowToPlay();
+                break;
+            case R.id.action_reset:
+                scoreComputer = 0;
+                scoreMe = 0;
+                displayScore();
+                storeWinnerScore(storeAndroidScore, scoreComputer);
+                storeWinnerScore(storeMeScore, scoreMe);
+                startNewGame();
+                break;
+            default:
+                break;
+            //return super.onOptionsItemSelected(item);
+        }
+        return false;
+    }
+
+    private void referenceToButton() {
+        button = new Button[numberOfButton];
+        //First Row
+        button[0] = (Button) findViewById(R.id.entry1);
+        button[1] = (Button) findViewById(R.id.entry2);
+        button[2] = (Button) findViewById(R.id.entry3);
+        //Second Row
+        button[3] = (Button) findViewById(R.id.entry4);
+        button[4] = (Button) findViewById(R.id.entry5);
+        button[5] = (Button) findViewById(R.id.entry6);
+        //Third Row
+        button[6] = (Button) findViewById(R.id.entry7);
+        button[7] = (Button) findViewById(R.id.entry8);
+        button[8] = (Button) findViewById(R.id.entry9);
+    }
+
+    private void displayScore() {
+        tvScoreComputer.setText(String.valueOf(scoreComputer));
+        tvScoreMe.setText(String.valueOf(scoreMe));
+    }
+
+    //Start a new Game
+    private void startNewGame() {
+        int k;
+        game.clearBoard();
+        for (k = 0; k < numberOfButton; k++) {
+            button[k].setText(" ");
+            button[k].setEnabled(true);
+            //Applied click listener to each button
+            button[k].setOnClickListener(new EntryButtonListener(k));
+        }
+    }
+
+    private class EntryButtonListener implements View.OnClickListener {
+        int mePosition;
+
+        public EntryButtonListener(int mePosition) {
+            this.mePosition = mePosition;
+        }
+
+        @Override
+        public void onClick(View v) {
+            button[mePosition].setText("x");
+            button[mePosition].setTextColor(getResources().getColor(R.color.Black));
+            game.storePlayerMove(mePosition, game.mePlayer());
+            button[mePosition].setEnabled(false);
+            disableButtons();
+            checkRunnableAdded = handler.postDelayed(displayAndroidMove, 1000);
+        }
+    }
+
+    private Runnable displayAndroidMove = new Runnable() {
+        @Override
+        public void run() {
+            int checkWinner;
+            checkWinner = game.checkWinner();
+            if (checkWinner == game.noWinnerYet()) {
+                int androidPosition = game.findComputerMove();
+                button[androidPosition].setText("o");
+                button[androidPosition].setTextColor(getResources().getColor(R.color.Black));
+                checkWinner = game.checkWinner();
+            }
+
+            if (checkWinner == game.mePlayerWin()) {
+                scoreMe = scoreMe + addAPoint;
+                tvScoreMe.setText(String.valueOf(scoreMe));
+                storeWinnerScore(storeMeScore, scoreMe);
+                showGameOverDialog(R.string.meWinner);
+            } else if (checkWinner == game.computerPlayerWin()) {
+                scoreComputer = scoreComputer + addAPoint;
+                tvScoreComputer.setText(String.valueOf(scoreComputer));
+                storeWinnerScore(storeAndroidScore, scoreComputer);
+                showGameOverDialog(R.string.androidWinner);
+            } else if (checkWinner == game.thereIsTie()) {
+                showGameOverDialog(R.string.tiedGame);//Show Dialog
+            } else {
+                enableButtons = game.getAllEntry();
+                for (int k = 0; k < numberOfButton; k++) {
+                    if (enableButtons[k] == game.emptyBoard()) {
+                        button[k].setEnabled(true);
+                    }
+                }
+            }
+        }
+    };
+
+    private void disableButtons() {
+        for (int k = 0; k < numberOfButton; k++) {
+            if (button[k].isEnabled()) {
+                //Disable button
+                button[k].setEnabled(false);
+            }
+        }
+    }
+
+    private void storeWinnerScore(String scoreKey, int scoreValue) {
+        SharedPreferences.Editor preferenceEditior = saveGameScore.edit();
+        preferenceEditior.putInt(scoreKey, scoreValue);
+        preferenceEditior.apply();
+    }
+
+    private void showGameOverDialog(final int messageId) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(GameActivity.this);
+        builder.setTitle(getResources().getString(R.string.gameTittle));
+        builder.setMessage(getResources().getString(messageId));
+        builder.setPositiveButton(R.string.OK, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startNewGame();
+            }
+        });
+        AlertDialog winnerNotify = builder.create();
+        winnerNotify.show();
+    }
+
+    private void showHowToPlay() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        dialog.setContentView(R.layout.pop_window_instruction);
+        dialog.setCanceledOnTouchOutside(true);
+        View masterView = dialog.findViewById(R.id.popWindow);
+        masterView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+
+    }
+
+    private boolean firstTimeShowInstruction() {
+        boolean ranBeforeLaunch = saveGameScore.getBoolean(checkFirsTime, false);
+        if (!ranBeforeLaunch) {
+            SharedPreferences.Editor editor = saveGameScore.edit();
+            editor.putBoolean(checkFirsTime, true);
+            editor.apply();
+        }
+        return !ranBeforeLaunch;
+    }
+
+    public void newGame(View v) {
+        if (checkRunnableAdded) {
+            handler.removeCallbacks(displayAndroidMove);//Stop postDelay
+            checkRunnableAdded = false;
+        }
+        startNewGame();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LinearLayout lnAd = (LinearLayout) findViewById(R.id.ln_ad);
+        if (Connectivity.isConnected(GameActivity.this)) {
+            lnAd.setVisibility(View.VISIBLE);
+        } else {
+            lnAd.setVisibility(View.GONE);
+        }
+    }
+}
---
+++ TicTacToe/app/src/main/java/tictactoe/ngontinhkangkang/com/tictactoe/GameEngine.java
@@ -0,0 +1,174 @@
+package tictactoe.ngontinhkangkang.com.tictactoe;
+
+import java.util.Random;
+
+public class GameEngine {
+    private int BOARD_SIZE;
+    private int[] entryBoard;
+    private Random randomAndroidPosition;
+    //Reset all entry
+    private final static int RESET_EMTY = 0;
+    //Players
+    private final static int ME_PLAYER = 1;
+    private final static int COMPUTER_PLAYER = 2;
+    //Case for winning
+    private final static int ME_PLAYER_WIN = 1;
+    private final static int COMPUTER_PLAYER_WIN = 2;
+    private final static int NO_WINNER_YET = 3;
+    private final static int THERE_IS_TIE = 4;
+
+    //Constructor
+    public GameEngine(int boardSize) {
+        this.BOARD_SIZE = boardSize;
+        entryBoard = new int[BOARD_SIZE];
+        clearBoard();
+        randomAndroidPosition = new Random();
+    }
+
+    //Clear all value in Array
+    public void clearBoard() {
+        for (int k = 0; k < BOARD_SIZE; k++) {
+            entryBoard[k] = RESET_EMTY;
+        }
+    }
+
+    //Store players move
+    public void storePlayerMove(int playerPosition, int whoIsPlayer) {
+        //WhoisPlayer:
+        //ME_PLAYER=1
+        //COMPUTER_PLAYER=2
+        entryBoard[playerPosition] = whoIsPlayer;
+    }
+
+    //Return all players move
+    public int[] getAllEntry() {
+        return entryBoard; //return all players move
+    }
+
+    //Find and return android player position
+    public int findComputerMove() {
+        int computerMove;
+        //Check if COMPUTER_PLAYER can win the game
+        for (int k = 0; k < BOARD_SIZE; k++) {
+            //Check an entry is empty
+            if (entryBoard[k] != ME_PLAYER && entryBoard[k] != COMPUTER_PLAYER) {
+                entryBoard[k] = COMPUTER_PLAYER;
+                //Check if Android wins
+                if (checkWinner() == COMPUTER_PLAYER_WIN) {
+                    return k;
+                } else {
+                    //Set entry back to empty
+                    entryBoard[k] = RESET_EMTY;
+                }
+            }
+        }
+
+        //Check if COMPUTER_PLAYER can block ME_PLAYER from winning
+        for (int k = 0; k < BOARD_SIZE; k++) {
+            //Check an entry is empty
+            if (entryBoard[k] != ME_PLAYER && entryBoard[k] != COMPUTER_PLAYER) {
+                entryBoard[k] = ME_PLAYER;
+                //Check if Android wins
+                if (checkWinner() == ME_PLAYER_WIN) {
+                    storePlayerMove(k, COMPUTER_PLAYER);
+                    return k;
+                } else {
+                    //Set entry back to empty
+                    entryBoard[k] = RESET_EMTY;
+                }
+            }
+        }
+
+        //Generate random COMPUTER_PLAYER position
+        //if none of the above is true.
+        //do-while statement uses less code
+        do {
+            computerMove = randomAndroidPosition.nextInt(BOARD_SIZE);
+        }
+        while (entryBoard[computerMove] == ME_PLAYER || entryBoard[computerMove] == COMPUTER_PLAYER);
+        //Store Android move
+        storePlayerMove(computerMove, COMPUTER_PLAYER);
+        return computerMove;
+    }
+
+    //Check winner and return who is the winner
+    public int checkWinner() {
+        //Return 1 if ME_PLAYER wins
+        //Return 2 if COMPUTER_PLAYER wins
+        //Return 3 if no winner yet
+        //Return 4 if there is a tie
+        int k;
+        //Check each column
+        for (k = 0; k < 3; k++) {
+            //Check if ME_PLAYER wins
+            if (entryBoard[k] == ME_PLAYER && entryBoard[k + 3] == ME_PLAYER && entryBoard[k + 6] == ME_PLAYER) {
+                return ME_PLAYER_WIN;
+            }
+            //Check if COMPUTER_PLAYER wins
+            if (entryBoard[k] == COMPUTER_PLAYER && entryBoard[k + 3] == COMPUTER_PLAYER && entryBoard[k + 6] == COMPUTER_PLAYER) {
+                return COMPUTER_PLAYER_WIN;
+            }
+        }
+        //Check each row
+        for (k = 0; k < 7; k += 3) {
+            //Check if ME_PLAYER wins
+            if (entryBoard[k] == ME_PLAYER && entryBoard[k + 1] == ME_PLAYER && entryBoard[k + 2] == ME_PLAYER) {
+                return ME_PLAYER_WIN;
+            }
+            //Check if COMPUTER_PLAYER wins
+            if (entryBoard[k] == COMPUTER_PLAYER && entryBoard[k + 1] == COMPUTER_PLAYER && entryBoard[k + 2] == COMPUTER_PLAYER) {
+                return COMPUTER_PLAYER_WIN;
+            }
+        }
+        //Check each diagonal
+        //Check if ME_PLAYER wins
+        if ((entryBoard[0] == ME_PLAYER && entryBoard[4] == ME_PLAYER && entryBoard[8] == ME_PLAYER) ||
+                (entryBoard[2] == ME_PLAYER && entryBoard[4] == ME_PLAYER && entryBoard[6] == ME_PLAYER)) {
+            return ME_PLAYER_WIN;
+        }
+
+        //Check if COMPUTER_PLAYER wins
+        if ((entryBoard[0] == COMPUTER_PLAYER && entryBoard[4] == COMPUTER_PLAYER && entryBoard[8] == COMPUTER_PLAYER) ||
+                (entryBoard[2] == COMPUTER_PLAYER && entryBoard[4] == COMPUTER_PLAYER && entryBoard[6] == COMPUTER_PLAYER)) {
+            return COMPUTER_PLAYER_WIN;
+        }
+
+        //Check for no winner yet
+        for (k = 0; k < BOARD_SIZE; k++) {
+            if (entryBoard[k] != ME_PLAYER && entryBoard[k] != COMPUTER_PLAYER) {
+                return NO_WINNER_YET;
+            }
+        }
+        //There is a tie game, no winner
+        return THERE_IS_TIE;
+    }
+
+    public int emptyBoard() {
+        return RESET_EMTY;
+    }
+
+    public int mePlayer() {
+        return ME_PLAYER;
+    }
+
+    public int androidPlayer() {
+        return COMPUTER_PLAYER;
+    }
+
+    public int mePlayerWin() {
+        return ME_PLAYER_WIN;
+    }
+
+    public int computerPlayerWin() {
+        return COMPUTER_PLAYER_WIN;
+    }
+
+    public int noWinnerYet() {
+        return NO_WINNER_YET;
+    }
+
+    public int thereIsTie() {
+        return THERE_IS_TIE;
+    }
+
+}
