e63f88238545f92d6db75c7e24d0123d68b20a03
v.loitp <v.Loitp@cloudhms.net>
2020-01-22 16:27:19 +0700

    update

--- TicTacToe/app/src/main/java/tictactoe/ngontinhkangkang/com/tictactoe/GameActivity.java
+++ TicTacToe/app/src/main/java/tictactoe/ngontinhkangkang/com/tictactoe/GameActivity.java
@@ -10,7 +10,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
---
+++ TicTacToe/app/src/main/java/tictactoe/ngontinhkangkang/com/tictactoe/LApplication.kt
@@ -0,0 +1,39 @@
+package tictactoe.ngontinhkangkang.com.tictactoe
+
+import androidx.multidex.MultiDexApplication
+import com.core.common.Constants
+import com.core.utilities.LConnectivityUtil
+import com.core.utilities.LUIUtil
+import com.data.ActivityData
+import com.data.AdmobData
+import com.github.piasy.biv.BigImageViewer
+import com.github.piasy.biv.loader.glide.GlideImageLoader
+import com.google.gson.Gson
+import com.utils.util.Utils
+
+class LApplication : MultiDexApplication() {
+    private val TAG = LApplication::class.java.simpleName
+
+    companion object {
+        val gson: Gson = Gson()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Constants.setIsDebug(true)
+        Utils.init(this)
+        //config admob id
+        AdmobData.instance.idAdmobFull = getString(R.string.str_f)
+        //config activity transition default
+        ActivityData.instance.type = Constants.TYPE_ACTIVITY_TRANSITION_SLIDELEFT
+
+        //config font
+        LUIUtil.fontForAll = Constants.FONT_PATH
+
+        //big imageview
+        BigImageViewer.initialize(GlideImageLoader.with(applicationContext))
+
+        LConnectivityUtil.initOnNetworkChange(applicationContext)
+    }
+}
